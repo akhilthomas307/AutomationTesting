eaa3c92832958b0bf541228f3f0378a36664bca8
akhilthomas307 <115609939+akhilthomas307@users.noreply.github.com>
2022-12-16 01:27:30 +0530

    Add files via upload

---
+++ MavenAssessment/src/main/java/org/ict/constants/AutomationConstants.java
@@ -0,0 +1,27 @@
+package org.ict.constants;
+
+public class AutomationConstants {
+	 
+			
+			public static String expusername="practice.swtesting@gmail.com";
+			public static String expPassword="123";
+			public static  String ExpectedValue=":: ICT ACADEMY OF KERALA ::";
+			
+
+			
+			
+		    public static String expname="Akhil K Thomas";
+			public static String exppassword="akhil@123";
+			public static String expemail="akhil@gmail.com";
+			public static String expdesignation="HR";
+			public static String expreportingto="Demo";
+			public static String expmemberof="Staff";
+		    public static String expempid="987760";
+			public static String exppassword2="akhil@123";
+			public static String expnumber="9687665556";
+			public static String exptype="Permanent";
+			public static String expaddress="Thrissur";	
+			
+		}
+
+
